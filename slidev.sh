b230a43d4a296180ed6a6c95356fa083ea2b218d
Bing Ran <bing.ran@icloud.com>
2025-01-14 15:29:12 +0800

    added exmplae in slidev.sh

--- slidev.sh
+++ slidev.sh
@@ -2,3 +2,5 @@
 
 # Pass all arguments to the bun command
 bun /Users/bran/localProjects/slidev/packages/slidev/node/cli.ts "$@"
+
+# example: slidev.sh --port 3030 --base /foo/ --viteFsAllow /Users/bran/localProjects/slidev/
